bc76bd5c7f490550d689ece8374dc2b6aecf9ff8
viveloper <usik1234@gmail.com>
2020-02-22 08:56:54 +0900

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import styles from '../css/footer.module.css'
+import links from '../constants/links'
+import socialIcons from '../constants/social-icons'
+import { Link } from 'gatsby'
 
 const Footer = () => {
   return (
-    <div>
-      <h1>i am footer</h1>
-    </div>
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        {links.map((item, index) => (
+          <Link key={index} to={item.path}>
+            {item.text}
+          </Link>
+        ))}
+      </div>
+      <div className={styles.icons}>
+        {socialIcons.map((item, index) => (
+          <a key={index} href={item.url} target="_blank">
+            {item.icon}
+          </a>
+        ))}
+      </div>
+      <div className={styles.copyright}>
+        copyright &copy; backroads travel company {new Date().getFullYear} all
+        right reserved
+      </div>
+    </footer>
   )
 }
 
