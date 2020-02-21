55c5de4d95df4b176823ed9ef57224d10e35b6c6
viveloper <usik1234@gmail.com>
2020-02-21 22:59:09 +0900

    added Navbar

---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const error = () => {
+  return <Layout>hello from error page.</Layout>
+}
+
+export default error
---
+++ src/pages/tours.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Layout from '../components/Layout'
+
+export class tours extends Component {
+  render() {
+    return <Layout>hello from tours page!!!!</Layout>
+  }
+}
+
+export default tours
