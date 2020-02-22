c8919b86fc03b958be1b7352a7ee91019f36f360
viveloper <ucsik1234@gmail.com>
2020-02-22 19:02:46 +0900

    gatsby image

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,27 @@
  */
 
 module.exports = {
-  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
+  siteMetadata: {
+    title: 'BackRoads',
+    description:
+      'Explore awesome woldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure',
+    author: '@johndoe',
+    data: {
+      name: 'john',
+      age: 25,
+    },
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
 }
--- src/pages/tours.js
+++ src/pages/tours.js
@@ -3,6 +3,7 @@ import Layout from '../components/Layout'
 
 export class tours extends Component {
   render() {
+    console.log(this.props.data)
     return <Layout>hello from tours page!!!!</Layout>
   }
 }
