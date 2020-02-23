4ecd93ef459f9e90922771ed03f8540f690de77a
viveloper <usik1234@gmail.com>
2020-02-23 23:23:09 +0900

    fixed home page background image(using gatsby-background-image)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import SimpleHero from '../components/SimpleHero'
+import StyledHero from '../components/StyledHero'
 import Banner from '../components/Banner'
 import { Link } from 'gatsby'
 import About from '../components/Home/About'
 import Services from '../components/Home/Services'
+import { graphql } from 'gatsby'
 
-export default () => (
+export default ({ data }) => (
   <Layout>
-    <SimpleHero>
+    <StyledHero img={data.defaultBcg.childImageSharp.fluid} home="true">
       <Banner
         title="continue exploring"
         info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, sapiente."
@@ -17,8 +18,20 @@ export default () => (
           explore tours
         </Link>
       </Banner>
-    </SimpleHero>
+    </StyledHero>
     <About />
     <Services />
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    defaultBcg: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
